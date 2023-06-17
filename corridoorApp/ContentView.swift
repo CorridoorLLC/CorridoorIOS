197e7160d82af06a8b1639848faf9e6dcbc56ad6
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-06-17 01:27:15 -0400

    created a conditional login button that I am going to use for the auth after i learn some swift ios design patterns

--- corridoorApp/ContentView.swift
+++ corridoorApp/ContentView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 struct ContentView: View {
+    @State var conditional = true
+    func buttonTapped() {
+        conditional = false
+    }
     var body: some View {
         //seeting the background gradient
         ZStack {
@@ -29,8 +33,14 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .lineLimit(0)
                     .frame(width: 200.0, height: 59)
-                Button("Button title") {
-                    print("Button tapped!")
+                if(conditional){
+                    Button(action: buttonTapped) {
+                        Text("Login")
+                        .padding()
+                        .background(Color(hue: 0.702, saturation: 0.0, brightness: 0.912, opacity: 0.759))
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                    }
                 }
             }
         }
